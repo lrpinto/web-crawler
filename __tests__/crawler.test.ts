1a745551e90a70a68dc3587cb331d78ce2f82a14
Luisa Pinto <luisa.pinto@togather.com>
2024-10-11 21:15:13 +0100

    Implement WebCrawler class with startUrl

--- __tests__/crawler.test.ts
+++ __tests__/crawler.test.ts
@@ -1,3 +1,11 @@
+import { WebCrawler } from '../src/crawler';
+
 test('Sample test for Jest setup', () => {
     expect(1 + 1).toBe(2);
 });
+
+test('Web Crawler class should be defined', async () => {
+    const crawler = new WebCrawler('https://example.com');
+    expect(crawler).toBeDefined();
+});
+
---
+++ src/crawler.ts
@@ -0,0 +1,8 @@
+export class WebCrawler {
+
+    startUrl: string;
+
+    constructor(startUrl: string) {
+        this.startUrl = startUrl;
+    }
+}
