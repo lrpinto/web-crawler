7999e7ebc7b607db5f4ecfd86803a8f7a3126202
Luisa Pinto <luisa.pinto@togather.com>
2024-10-14 09:56:40 +0100

    Add docs to cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,26 @@
 import { WebCrawler } from "./crawler";
 import { Logger } from "./logger";
 
+/**
+ * `startUrl` is the URL where the web crawler should start crawling.
+ * It is passed as a command line argument when running the script.
+ */
 const startUrl = process.argv[2];
 
+/**
+ * If no start URL is provided, log an error message and exit the process with a status code of 1.
+ */
 if (!startUrl) {
   Logger.error("Please provide a start URL");
   process.exit(1);
 }
 
+/**
+ * `runCrawler` is an asynchronous function that creates a new instance of `WebCrawler` and starts the crawling process.
+ * After the crawling process is finished, it logs the number of pages visited and the URLs of the visited pages.
+ *
+ * @param {string} startUrl - The URL where the web crawler should start crawling.
+ */
 const runCrawler = async (startUrl: string) => {
   const crawler = new WebCrawler(startUrl);
   const visitedUrls = await crawler.crawl();
@@ -16,4 +29,7 @@ const runCrawler = async (startUrl: string) => {
   Logger.info([...visitedUrls].join("\n"));
 };
 
+/**
+ * Starts the crawling process by calling the `runCrawler` function with the `startUrl` as an argument.
+ */
 runCrawler(startUrl);
